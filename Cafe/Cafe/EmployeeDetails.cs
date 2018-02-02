a5de6af7840bf82507091866baedf806b65eca11
Emily Blake <Emily Blake@DESKTOP-RMTPEV9>
2018-02-02 19:59:33 +0000

    third commit

---
+++ Cafe/Cafe/EmployeeDetails.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cafe
+{
+    public class EmployeeDetails
+    {
+        public int ID;
+        public string FullName;
+        public string UserName;
+        public string Password; 
+        public EmployeeDetails()
+        {
+
+        }
+        public EmployeeDetails(int ID, string FullName, string UserName, string Password)
+        {
+           
+        }
+        /*
+        { set; get; }
+        */
+    }
+    }
+   
+   
+
--- Cafe/Cafe/Employees.cs
+++ Cafe/Cafe/Employees.cs
@@ -10,18 +10,27 @@ using System.Windows.Forms;
 
 namespace Cafe
 {
+   
     public partial class Employees : Form
     {
         public Employees()
         {
             InitializeComponent();
+            
         }
+        public List<EmployeeDetails> EmployeeDetails;
 
         private void Save_Click(object sender, EventArgs e)
         {
             if (IDText.Text == "" || FullNameText.Text == "" || UserNameText.Text == "" || PasswordText.Text == "")
             {
                 MessageBox.Show("Please fill in all the boxes");
+            }
+            else
+            {
+                List<EmployeeDetails> EmployeeDetails = new List<EmployeeDetails>();
+                EmployeeDetails.Add(int.Parse(IDText.Text), FullNameText.Text, UserNameText.Text, PasswordText.Text);
+                MessageBox.Show ("Employee has been added to the system");
             }
         }
     }
