3bd253b881568741eefc32c777d072ac6aea5c27
Emily Blake <Emily Blake@DESKTOP-RMTPEV9>
2018-02-08 21:03:06 +0000

    Starting to load in reading from textfile

--- Cafe/Cafe/Login.cs
+++ Cafe/Cafe/Login.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,9 +18,40 @@ namespace Cafe
             InitializeComponent();
         }
 
+        public void ReadingTextFile()
+        {
+            const string textfile = "Savedlist.txt";
+
+            using (StreamReader r = new StreamReader(textfile))
+            {
+               string line = " ";
+                string Password = " ";
+                string UserName = " ";
+                string FullName= " ";
+                string ID = "";
+                while ((line = r.ReadLine()) != null)
+                {
+                    string[] linearray = line.Split(',');
+                    for (int i = 0; i < 1; i++)
+                    {
+                        Password = linearray[i+2];
+                        UserName = linearray[i+1];
+                        FullName = linearray[i];
+                        ID = linearray[i++];
+                    }
+                    foreach(string linesplit in linearray)
+                    {
+                                                                     
+                    }
+                   List<EmployeeDetails> Listofemployees = new List<EmployeeDetails>();
+                    Listofemployees.Add(new EmployeeDetails(int.Parse(ID), FullName, UserName, Password)); 
+                }
+            }
+        }
+
         private void Enter_Click(object sender, EventArgs e)
         {
-
+            ReadingTextFile();
         }
     }
 }
--- Cafe/Cafe/Program.cs
+++ Cafe/Cafe/Program.cs
@@ -16,7 +16,7 @@ namespace Cafe
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Employees());
+            Application.Run(new Login());
         }
     }
 }
